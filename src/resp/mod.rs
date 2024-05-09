6d14f35742557eba424b056ddfe012d9a2e06e87
FMhyxeee <1179570770@qq.com>
2024-05-10 01:31:41 +0800

    finish resp and encode trait.

---
+++ src/resp/mod.rs
@@ -0,0 +1,280 @@
+mod decode;
+mod encode;
+
+use std::{
+    collections::BTreeMap,
+    ops::{Deref, DerefMut},
+};
+
+use enum_dispatch::enum_dispatch;
+
+#[enum_dispatch]
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode {
+    fn decode(self) -> Result<RespFrame, String>;
+}
+
+#[enum_dispatch(RespEncode)]
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum RespFrame {
+    SimpleString(SimpleString),
+    Error(SimpleError),
+    Integer(i64),
+    BulkString(BulkString),
+    NullBulkString(RespNullBulkString),
+    Array(RespArray),
+    NullArray(RespNullArray),
+    Null(RespNull),
+
+    Boolean(bool),
+    Double(f64),
+    Map(RespMap),
+    Set(RespSet),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct SimpleString(String);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct SimpleError(String);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct BulkString(Vec<u8>);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct RespNullBulkString;
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespArray(Vec<RespFrame>);
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespNullArray;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct RespNull;
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespMap(BTreeMap<String, RespFrame>);
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespSet(Vec<RespFrame>);
+
+impl Deref for SimpleString {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for SimpleError {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for BulkString {
+    type Target = Vec<u8>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespArray {
+    type Target = Vec<RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespMap {
+    type Target = BTreeMap<String, RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for RespMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Deref for RespSet {
+    type Target = Vec<RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl SimpleString {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleString(s.into())
+    }
+}
+
+impl SimpleError {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleError(s.into())
+    }
+}
+
+impl BulkString {
+    pub fn new(v: impl Into<Vec<u8>>) -> Self {
+        BulkString(v.into())
+    }
+}
+
+impl RespArray {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        RespArray(s.into())
+    }
+}
+
+impl RespMap {
+    pub fn new() -> Self {
+        RespMap(BTreeMap::new())
+    }
+}
+
+impl Default for RespMap {
+    fn default() -> Self {
+        RespMap::new()
+    }
+}
+
+impl RespSet {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        RespSet(s.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::RespFrame;
+
+    use super::*;
+
+    #[test]
+    fn test_simple_string_encode() {
+        let frame: RespFrame = SimpleString::new("OK".to_string()).into();
+
+        assert_eq!(frame.encode(), b"+OK\r\n");
+    }
+
+    #[test]
+    fn test_error_encode() {
+        let frame: RespFrame = SimpleError::new("Error message".to_string()).into();
+
+        assert_eq!(frame.encode(), b"-Error message\r\n");
+    }
+
+    #[test]
+    fn test_integer_encode() {
+        let frame: RespFrame = 123.into();
+        assert_eq!(frame.encode(), b":+123\r\n");
+
+        let frame: RespFrame = (-123).into();
+        assert_eq!(frame.encode(), b":-123\r\n");
+    }
+
+    #[test]
+    fn test_bulk_string_encode() {
+        let frame: RespFrame = BulkString::new(b"hello".to_vec()).into();
+        assert_eq!(frame.encode(), b"$5\r\nhello\r\n");
+    }
+
+    #[test]
+    fn test_null_bulk_string_encode() {
+        let frame: RespFrame = RespNullBulkString.into();
+        assert_eq!(frame.encode(), b"$-1\r\n");
+    }
+
+    #[test]
+    fn test_array_encode() {
+        let frame: RespFrame = RespArray::new(vec![
+            BulkString::new("set".to_string()).into(),
+            BulkString::new("hello".to_string()).into(),
+            BulkString::new("world".to_string()).into(),
+        ])
+        .into();
+        assert_eq!(
+            &frame.encode(),
+            b"*3\r\n$3\r\nset\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
+        );
+    }
+
+    #[test]
+    fn test_null_array_encode() {
+        let frame: RespFrame = RespNullArray.into();
+        assert_eq!(frame.encode(), b"*-1\r\n");
+    }
+
+    #[test]
+    fn test_null_encode() {
+        let frame: RespFrame = RespNull.into();
+        assert_eq!(frame.encode(), b"_\r\n");
+    }
+
+    #[test]
+    fn test_boolean_encode() {
+        let frame: RespFrame = true.into();
+        assert_eq!(frame.encode(), b"#t\r\n");
+
+        let frame: RespFrame = false.into();
+        assert_eq!(frame.encode(), b"#f\r\n");
+    }
+
+    #[test]
+    fn test_double_encode() {
+        let frame: RespFrame = 123.456.into();
+        assert_eq!(frame.encode(), b",+123.456\r\n");
+
+        let frame: RespFrame = (-123.456).into();
+        assert_eq!(frame.encode(), b",-123.456\r\n");
+
+        let frame: RespFrame = 1.23456e+8.into();
+        assert_eq!(frame.encode(), b",+1.23456e8\r\n");
+
+        let frame: RespFrame = (-1.23456e-9).into();
+        assert_eq!(&frame.encode(), b",-1.23456e-9\r\n");
+    }
+
+    #[test]
+    fn test_map_encode() {
+        let mut map = RespMap::new();
+        map.insert(
+            "hello".to_string(),
+            BulkString::new("world".to_string()).into(),
+        );
+        map.insert("foo".to_string(), (-123456.789).into());
+
+        let frame: RespFrame = map.into();
+        assert_eq!(
+            &frame.encode(),
+            b"%2\r\n+foo\r\n,-123456.789\r\n+hello\r\n$5\r\nworld\r\n"
+        );
+    }
+
+    #[test]
+    fn test_set_encode() {
+        let frame: RespFrame = RespSet::new([
+            RespArray::new([1234.into(), true.into()]).into(),
+            BulkString::new("world".to_string()).into(),
+        ])
+        .into();
+        assert_eq!(
+            frame.encode(),
+            b"~2\r\n*2\r\n:+1234\r\n#t\r\n$5\r\nworld\r\n"
+        );
+    }
+}
